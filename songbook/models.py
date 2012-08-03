dd096f5cb12e0ab06c182e3465a16045f6b364e5
Tomasz Pazurkiewicz <t.pazurkiewicz@gmail.com>
2012-08-04 01:50:09 +0200

    Add fields to Song model.

--- songbook/models.py
+++ songbook/models.py
@@ -7,7 +7,12 @@ class Song(models.Model):
             'lyrics': self.lyrics,
         }
     spreadsheet_fields = ['title', 'lyrics']
+
     title = models.CharField(max_length=255)
     lyrics = models.TextField()
-    comment = models.TextField()
-    lyrics_author = models.CharField(max_length=255)
+    comment = models.TextField(blank=True, null=True)
+    info = models.TextField(blank=True, null=True)
+    lyrics_author = models.CharField(max_length=255, blank=True, null=True)
+    music_author = models.CharField(max_length=255, blank=True, null=True)
+    music_year = models.SmallIntegerField(blank=True, null=True)
+    lyrics_year = models.SmallIntegerField(blank=True, null=True)
