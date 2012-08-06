b6a228a4d828219598e3f49c3e5278c97524fdde
Tomasz Pazurkiewicz <t.pazurkiewicz@gmail.com>
2012-08-06 23:33:08 +0200

    Add Songbook to admin,.

--- songbook/admin.py
+++ songbook/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 
 from songbook import models
 
+class SongBookAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(models.Song)
+admin.site.register(models.SongBook)
