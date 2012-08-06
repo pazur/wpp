de4666f8622b1ebcaae9ccc7bf5f91bbc9102355
Tomasz Pazurkiewicz <t.pazurkiewicz@gmail.com>
2012-08-06 23:29:40 +0200

    Add staticfiles.

--- wpp/settings.py
+++ wpp/settings.py
@@ -33,11 +33,14 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.auth',
     'django.contrib.sessions',
+    'django.contrib.staticfiles',
     'djangotoolbox',
     'autoload',
     'dbindexer',
     'filetransfers',
     'songbook',
+
+    'wpp',
 
     # djangoappengine should come last, so it can override a few manage.py commands
     'djangoappengine',
@@ -47,6 +50,7 @@ MIDDLEWARE_CLASSES = (
     # This loads the index definitions, so it has to come first
     'autoload.middleware.AutoloadMiddleware',
 
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -56,6 +60,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
     'django.core.context_processors.request',
     'django.core.context_processors.media',
+    'django.core.context_processors.static',
 )
 
 ADMIN_MEDIA_PREFIX = '/media/admin/'
@@ -64,3 +69,8 @@ TEMPLATE_DIRS = (os.path.join(os.path.dirname(__file__), 'templates'),)
 ROOT_URLCONF = 'wpp.urls'
 
 DEFAULT_FILE_STORAGE = 'gaeblob_storage.backends.BlobPropertyStorage'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = '/tmp/wpp-static'
+
+LOGIN_URL='/login/'
