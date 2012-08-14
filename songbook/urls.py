064d397087fa3dc30550d2843c317b5b9df9920b
Tomasz Pazurkiewicz <t.pazurkiewicz@gmail.com>
2012-08-15 00:01:27 +0200

    Add songbook latex view.

--- songbook/urls.py
+++ songbook/urls.py
@@ -10,5 +10,7 @@ urlpatterns = patterns('songbook.views',
     url(r'^add_songbook/', 'songbook_create_view', name="add_songbook"),
     url(r'^update_songbook/(?P<pk>[0-9]+)/$', 'songbook_update_view', name="update_songbook"),
     url(r'^songbook/(?P<pk>[0-9]+)/$', 'songbook_detail_view', name="songbook"),
+    url(r'^songbook/(?P<pk>[0-9]+)/latex/$', 'songbook_latex_view', name="songbook_latex"),
+    url(r'^songbook/(?P<pk>[0-9]+)/latex/hash/$', 'songbook_latex_hash_view', name="songbook_latex_hash"),
     url(r'^songbook_list/$', 'songbook_list_view', name="songbook_list"),
 )
--- songbook/views.py
+++ songbook/views.py
@@ -1,5 +1,10 @@
+import hmac
+
 from django.contrib.auth import decorators
+from django.conf import settings
+from django.core import exceptions
 from django import http
+from django.views.decorators.csrf import csrf_exempt
 from django.views import generic
 from django.views.generic import detail
 
@@ -18,9 +23,7 @@ class SongCreateView(generic.CreateView):
 class SongDetailView(generic.DetailView):
     model = models.Song
 
-class SongLatexView(detail.SingleObjectMixin, generic.View):
-    model = models.Song
-
+class LatexView(detail.SingleObjectMixin, generic.View):
     def get(self, request, *args, **kwargs):
         object = self.get_object()
         latex = object.latex_preview()
@@ -38,11 +41,26 @@ class SongbookUpdateView(AllSongsMixin, generic.UpdateView):
 class SongbookCreateView(AllSongsMixin, generic.CreateView):
     model = models.SongBook
 
+class SongBookLatexHashView(LatexView):
+    http_method_names = ['post']
+    model = models.SongBook
+
+    def post(self, request, *args, **kwargs):
+        hash = sefl.request.POST.get('hash', None)
+        expected = hmac.new(settings.LATEX_SECRET, request.build_absolute_uri())
+        if hash != expected:
+            raise exceptions.PermissionDenied
+        return self.get(request, *args, **kwargs)
+
+
 song_list_view = decorators.login_required(SongListView.as_view())
 song_update_view = decorators.login_required(SongUpdateView.as_view())
 song_create_view = decorators.login_required(SongCreateView.as_view())
 song_detail_view = decorators.login_required(SongDetailView.as_view())
-song_latex_view = decorators.login_required(SongLatexView.as_view())
+
+song_latex_view = decorators.login_required(LatexView.as_view(model=models.Song))
+songbook_latex_view = decorators.login_required(LatexView.as_view(model=models.SongBook))
+songbook_latex_hash_view = csrf_exempt(SongBookLatexHashView.as_view())
 
 songbook_list_view = decorators.login_required(generic.ListView.as_view(model=models.SongBook))
 songbook_create_view = decorators.login_required(SongbookCreateView.as_view())
